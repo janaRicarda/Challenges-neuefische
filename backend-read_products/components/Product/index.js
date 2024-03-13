7e63d4983462d633f954bf0462fbc80b0d9a763f
Jana May <jana.ricarda.may@gmail.com>
2024-03-13 17:11:14 +0100

    almost solved

---
+++ backend-read_products/components/Product/index.js
@@ -0,0 +1,45 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { ProductCard } from "./Product.styled";
+import { StyledLink } from "../Link/Link.styled";
+
+export default function Product() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, isLoading } = useSWR(`/api/products/${id}`);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  return (
+    <ProductCard>
+      <h2>{data.name}</h2>
+      <p>Description: {data.description}</p>
+      <p>
+        Price: {data.price} {data.currency}
+      </p>
+      <h3>Reviews:</h3>
+      {data.reviews.length ? (
+        <ul>
+          {data.reviews.map((review) => {
+            <li key={review.id}>
+              <p>
+                {review.title} - {review.text}
+              </p>
+              <p>Rating: {review.rating}</p>
+            </li>;
+          })}
+        </ul>
+      ) : (
+        <p>No reviews</p>
+      )}
+      <StyledLink href="/">Back to all</StyledLink>
+    </ProductCard>
+  );
+}
---
+++ backend-read_products/pages/api/products/[id].js
@@ -0,0 +1,20 @@
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
+
+export default async function handler(request, response) {
+  const { id } = request.query;
+
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const product = await Product.findById(id).populate("reviews");
+    if (!product) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+    response.status(200).json(product);
+    /*  return response.status(200).json(products);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  } */
+  }
+}
